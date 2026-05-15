2375b25ef0f9e479c5fedba54729834e0d26c9de
Anu <anu03192005@gmail.com>
2026-05-15 14:47:41 +0530

    folder naming is corrected

--- sangha/src/app/sangha/layout.tsx
+++ sangha/src/app/sangha/layout.tsx
@@ -15,7 +15,7 @@ const navigation = [
   { name: "Pending Users",   href: "/sangha/pending-users",   icon: Clock },
   { name: "Member Requests", href: "/sangha/member-requests", icon: Users },
   { name: "Sangha Members",  href: "/sangha/members",         icon: Users },
-  { name:"scholarships",     href:"/sangha/scholarships",       icon:Users},
+  { name:"scholarships",     href:"/sangha/scholarships",      icon:Users},
   { name: "Analytics",         href: "/sangha/reports",         icon: FileText },
   { name: "Activity Logs",   href: "/sangha/activity-logs",   icon: Activity },
   { name: "Block Users",     href: "/sangha/block",           icon: ShieldOff },
